dafd6d13db3a9d62429723599121a73f8d6de225
Richard James Childress <rjc8kt@virginia.edu>
2019-03-02 19:52:58 -0500

    Yo we out here just figuring out js like it's nuthin. We just put an entire html file into a variable. Now that's epic! :D

---
+++ frontend/testScript.js
@@ -0,0 +1,25 @@
+document.getElementById("analyze").onclick = function() {myFunction()};
+
+function myFunction() {
+
+	console.log( "poopy 3" );
+
+	let params = {
+		active: true,
+		currentWindow: true
+	}
+
+	chrome.tabs.query( params, gotTabs );
+
+	function gotTabs( tabs ) {
+		let msg = {
+			txt: "hello"
+		}
+		console.log(tabs[0]);
+		chrome.tabs.sendMessage( tabs[0].id, msg );
+	}
+
+
+
+	
+}
